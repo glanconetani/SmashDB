d442653e02a73a1212acb004a555901b96635771
jknandre <jna682@utexas.edu>
2019-10-13 12:17:59 -0500

    include about

--- app.js
+++ app.js
@@ -55,8 +55,19 @@ var app = http.createServer(function (req, res) {
       }
       res.end();
     });
-  }
-  else {
+  }else if(req.url.indexOf('about') != -1){
+    fs.readFile(__dirname + '/public/about.html', function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: Resource not found.');
+        console.log(err);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+      }
+      res.end();
+    });
+  }else {
     fs.readFile(__dirname + '/public/homepage.html', function (err, data) {
       if (err) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
